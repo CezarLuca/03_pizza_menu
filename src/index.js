deaaf2aa70a6b5fe2fd6a2936ea214038708658a
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-02-03 14:51:47 +0100

    Passing and Receiving Props done

--- src/index.js
+++ src/index.js
@@ -76,10 +76,39 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <Pizza />
-            <Pizza />
-            <Pizza />
+            <Pizza
+                name="Pizza Prosciutto"
+                ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+                photoName="pizzas/prosciutto.jpg"
+                price={12}
+            />
+            <Pizza
+                name="Pizza Margherita"
+                ingredients="Tomato and mozarella"
+                photoName="pizzas/margherita.jpg"
+                price={8}
+            />
+            <Pizza
+                name="Pizza Funghi"
+                ingredients="Tomato, mozarella, mushrooms, and onion"
+                photoName="pizzas/funghi.jpg"
+                price={10}
+            />
         </main>
+    );
+}
+
+function Pizza(props) {
+    console.log(props);
+    return (
+        <div className="pizza">
+            <img src={props.photoName} alt={props.name} />
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price + 1} €</span>
+            </div>
+        </div>
     );
 }
 
@@ -92,7 +121,7 @@ function Footer() {
     const openHour = 10;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
-    console.log(isOpen);
+    // console.log(isOpen);
     if (!isOpen) {
         return (
             <footer className="footer">
@@ -119,16 +148,6 @@ function Footer() {
     // );
 
     // return React.createElement("footer", null, "© 2024 Fast React Pizza GmbH.");
-}
-
-function Pizza() {
-    return (
-        <div>
-            <img src="pizzas/prosciutto.jpg" alt="Pizza Prosciutto" />
-            <h3>Pizza Prosciutto</h3>
-            <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-        </div>
-    );
 }
 
 // React v18
